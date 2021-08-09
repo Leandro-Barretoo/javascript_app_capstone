70014abe82b7826fc2c21989fa9eae3b78d8eef8
Leandro <officialdevmi@gmail.com>
2021-08-09 10:18:52 -0700

    Add assets and header

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,24 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+            {
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'assets/images/'
+                }
+            }
+        ]
+      },
+      {
+          test: /\.html$/,
+          use: [
+              'html-loader'
+          ]
+      },
     ],
   },
 };
