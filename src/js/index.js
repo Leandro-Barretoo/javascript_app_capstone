6ad302897c81e3636d7f5f1af4c2ce9956a4bd08
Leandro <officialdevmi@gmail.com>
2021-08-09 10:39:39 -0700

    Fix last linter errors

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import '../scss/style.scss';
 
-//const url = 'https://api.imgflip.com/get_memes';
+// const url = 'https://api.imgflip.com/get_memes';
 
-//fetch(url)
+// fetch(url)
 //  .then((response) => response.json())
 //  .then((json) => console.log(json.data.memes[88].url));
