4d622cbb67941a0cad95416c4f675d2664663ed4
Leandro <officialdevmi@gmail.com>
2021-08-11 05:46:18 -0700

    Fix radix parameter

--- src/js/likes.js
+++ src/js/likes.js
@@ -1,5 +1,6 @@
 const postLike = (itemID) => {
   fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/GuhYPExBlAlZ5RuYo3CN/likes', {
+    //GuhYPExBlAlZ5RuYo3CN
     method: 'POST',
     body: JSON.stringify({ item_id: itemID }),
     headers: {
@@ -16,7 +17,7 @@ const checkElems = () => {
       const idOfItem = e.target.className.split(' ')[0];
       const nextSib = e.target.nextSibling;
       postLike(idOfItem);
-      nextSib.innerHTML = parseInt(nextSib.innerHTML, 2) + 1;
+      nextSib.innerHTML = parseInt(nextSib.innerHTML, 10) + 1;
     });
   });
 };
