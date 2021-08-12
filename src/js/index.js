10a721a5d59fba62b9fb6b69753a4757f4dac2d1
ahmetbozaci <ahmt9417@gmail.com>
2021-08-12 18:11:42 +0300

    Create comment dom

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/style.scss';
+// import '../scss/style.scss';
 import addToGrid, { displayLikes } from './display.js';
 import checkElems from './likes.js';
 import displayData from './displayData.js';
@@ -8,3 +8,17 @@ import getNumElems from './homeCounter.js';
 addToGrid(checkElems, getNumElems);
 displayData(popup);
 displayLikes();
+
+// fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/GuhYPExBlAlZ5RuYo3CN/comments', {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     item_id: 61581,
+//     username: 'Ahmet',
+//     comment: 'This so funny',
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => {console.log(json)});
